4747b78025ffefb7c84c413e577c31e5fd14b7b3
santi <santirod520@gmail.com>
2023-12-22 18:43:49 -0800

    Added comments and used `splitext` to remove file extension from list.

--- FileName/FileName.py
+++ FileName/FileName.py
@@ -1,28 +1,33 @@
+# Import necessary packages.
 import os
+from openpyxl import Workbook
+wb = Workbook()
 
+ws = wb.active
+
+# Start/stop condition.
 convert = 'y'
 
 while convert == "y":
     try:
+        # Enter filepath and list files in said directory.
         filepath = input("Please enter your file path:")
         entries = os.listdir(filepath)
         print('\n')
         
-        for entry in entries:
-            print(entry)
+        # Print file names.
+        print("The following files were added to the CD Nexus.")
+        for index, entry in enumerate(entries):
+            print(os.path.splitext(entry)[0])
+            doc_name = ws.cell(row = index + 1 , column = 2 , value = os.path.splitext(entry)[0])
         print('\n')
-        check_periods = input("Except for the file type, are there any files with a \".\" in its name? (Y/N):")
-        print('\n')
-
-        check_periods: check_periods.strip().lower()
-        sep = '.'
-
-        if check_periods == 'n':
-            for entry in entries:
-                print(entry.partition(sep)[0])
-    
+        
+        #Update `convert` to continue or stop loop
         convert = input('Do you have any other files names you wish to extract? (Y/N):')
-
+        
+        #Save the CD Nexus in desired destination
+        wb.save('C:\\Users\\santi\\source\\repos\\FileName\\test.xlsx')
+    #Output if a filepath is not entered
     except WindowsError:
         print('Bruh, that\'s not a filepath, try again.')
 
