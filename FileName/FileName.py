6f18dbe50f19112dbb5fdd6ea6229da555efef7b
santi <santirod520@gmail.com>
2023-12-22 19:14:05 -0800

    Allow to continuously add file names in one instance

--- FileName/FileName.py
+++ FileName/FileName.py
@@ -8,6 +8,9 @@ ws = wb.active
 # Start/stop condition.
 convert = 'y'
 
+# `Python` starts at need to add 1 to start at appropriate cell on Excel.
+row_index = 1
+
 while convert == "y":
     try:
         # Enter filepath and list files in said directory.
@@ -19,14 +22,17 @@ while convert == "y":
         print("The following files were added to the CD Nexus.")
         for index, entry in enumerate(entries):
             print(os.path.splitext(entry)[0])
-            doc_name = ws.cell(row = index + 1 , column = 2 , value = os.path.splitext(entry)[0])
+            doc_name = ws.cell(row = index + row_index , column = 2 , value = os.path.splitext(entry)[0])
+            row = index + row_index
         print('\n')
         
+        #When iterating index is reset to 0, need the 1 to start at the appropriate cell.
+        row_index = row + 1
         #Update `convert` to continue or stop loop
         convert = input('Do you have any other files names you wish to extract? (Y/N):')
         
         #Save the CD Nexus in desired destination
-        wb.save('C:\\Users\\santi\\source\\repos\\FileName\\test.xlsx')
+        wb.save('your filepath')
     #Output if a filepath is not entered
     except WindowsError:
         print('Bruh, that\'s not a filepath, try again.')
